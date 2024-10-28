00efed28650504527341f3268439732f41a21603
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2024-10-28 14:49:53 +0530

    setup: vite

---
+++ client/README.md
@@ -0,0 +1,5 @@
+
+
+## Attributions
+
+1. robot.png: <a href="https://www.flaticon.com/free-icons/robot" title="robot icons">Robot icons created by Smashicons - Flaticon</a>
--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
+app.use((req, res) => res.status(404).sendFile(path.join(__dirname, 'public', '404.html')));
 
 app.listen(port, () => console.log(`app is listening on port ${port}`));
