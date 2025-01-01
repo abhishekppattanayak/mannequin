c1f2e0863f7d7f9ba2856d5c65aae954ec3bfa7a
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2025-01-02 00:50:31 +0530

    add: 404 page

--- app/src/routes/[...404].tsx
+++ app/src/routes/[...404].tsx
@@ -1,3 +1,18 @@
+import { A } from "@solidjs/router";
+
 export default function NotFound() {
-	return <main></main>;
+	return (
+		<main class="h-screen grid place-content-center *:text-center dark:bg-neutral-900 dark:text-white " >
+			<h1 class="text-lg md:text-2xl font-semibold" >
+				<span class="text-red-400" >Error 404:</span>
+				{" "}
+				<span>Page Not Found :P</span>
+			</h1>
+			<h1 class="text-md md:text-xl" >
+				<span>Go back to</span>
+				{" "}
+				<A href="/" class="text-blue-600 dark:text-blue-400 hover:underline" >Home</A>
+			</h1>
+		</main>
+	);
 }
